5e0e074070dbef58ebb6ee0069e9015c64cbae9f
peter <haikalprasetya.alhakim12@gmail.com>
2024-07-27 09:22:54 +0700

    expressjs and middleware

---
+++ middleware/logEvents.js
@@ -0,0 +1,33 @@
+import { format } from "date-fns";
+import { v4 as uuid } from "uuid";
+import fs from "fs";
+import path from "path";
+import fsPromises from "fs/promises";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const logEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    }
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logName),
+      logItem
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "test.txt");
+  console.log(`${req.method}-${req.headers.origin}-${req.url}`);
+  next();
+};
